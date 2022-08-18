e17ba6d4dc7a785683b72bbe2973430ff88f6afb
ArthurGini <arthurbriganti@hotmail.com>
2022-08-17 21:40:00 -0300

    Att de packs, window dos dados, cnn, lstm

--- 2_Transform/1_Pyspark_union_df.py
+++ 2_Transform/1_Pyspark_union_df.py
@@ -3,50 +3,35 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.appName('TCC ai bro').getOrCreate()
+spark = SparkSession.builder.appName("TCC ai bro").getOrCreate()
 
 
-df_ibova = spark.read.option('header',True).csv('ibova.csv')
+df_ibova = spark.read.option("header", True).csv("ibova.csv")
 df_ibova = df_ibova.select(
-	col('Open').alias('open_ibova'),
-	col('Date').alias('date_ibova')
+    col("Open").alias("open_ibova"), col("Date").alias("date_ibova")
 )
 
-df_oil = spark.read.option('header',True).csv('oil.csv')
-df_oil = df_oil.select(
-	col('Open').alias('open_oil'),
-	col('Date').alias('date_oil')
-)
+df_oil = spark.read.option("header", True).csv("oil.csv")
+df_oil = df_oil.select(col("Open").alias("open_oil"), col("Date").alias("date_oil"))
 
-df_usdbrl = spark.read.option('header',True).csv('usdbrl.csv')
+df_usdbrl = spark.read.option("header", True).csv("usdbrl.csv")
 df_usdbrl = df_usdbrl.select(
-	col('Open').alias('open_usd'),
-	col('Date').alias('date_usd')
+    col("Open").alias("open_usd"), col("Date").alias("date_usd")
 )
 
-df_abev = spark.read.option('header',True).csv('Open_ABEV3.SA.csv')
-df_abev = df_abev.select(
-	col('Open').alias('open_abev'),
-	col('Date').alias('date_abev')
-)
+df_abev = spark.read.option("header", True).csv("Open_ABEV3.SA.csv")
+df_abev = df_abev.select(col("Open").alias("open_abev"), col("Date").alias("date_abev"))
 
-df_jbs = spark.read.option('header',True).csv('Open_JBSAY.csv')
-df_jbs = df_jbs.select(
-	col('Open').alias('open_jbs'),
-	col('Date').alias('date_jbs')
-)
+df_jbs = spark.read.option("header", True).csv("Open_JBSAY.csv")
+df_jbs = df_jbs.select(col("Open").alias("open_jbs"), col("Date").alias("date_jbs"))
 
-df_petr4 = spark.read.option('header',True).csv('Open_PETR4.csv')
+df_petr4 = spark.read.option("header", True).csv("Open_PETR4.csv")
 df_petr4 = df_petr4.select(
-	col('Open').alias('open_petr4'),
-	col('Date').alias('date_petr4')
+    col("Open").alias("open_petr4"), col("Date").alias("date_petr4")
 )
 
-df_vale = spark.read.option('header',True).csv('Open_PETR4.csv')
-df_vale = df_vale.select(
-	col('Open').alias('open_vale'),
-	col('Date').alias('date_vale')
-)
+df_vale = spark.read.option("header", True).csv("Open_PETR4.csv")
+df_vale = df_vale.select(col("Open").alias("open_vale"), col("Date").alias("date_vale"))
 
 ibova = df_ibova.count()
 oil = df_oil.count()
@@ -58,7 +43,8 @@ jbs = df_jbs.count()
 petr4 = df_petr4.count()
 vale = df_vale.count()
 
-print(f'''\b Controle:
+print(
+    f"""\b Controle:
 	ibova = {ibova}
 	oil = {oil}
 	sp500 = {sp500}
@@ -68,7 +54,8 @@ print(f'''\b Controle:
 	jbs = {jbs}
 	petr4 = {petr4}
 	vale = {vale}
-''')
+"""
+)
 
 df = df_ibova.join(df_oil, df_oil.date_oil == df_ibova.date_ibova)
 df = df.join(df_usdbrl, df_usdbrl.date_usd == df_ibova.date_ibova)
@@ -78,16 +65,17 @@ df = df.join(df_petr4, df_petr4.date_petr4 == df_ibova.date_ibova)
 df = df.join(df_vale, df_vale.date_vale == df_ibova.date_ibova)
 
 df = df.select(
-	col('date_ibova').alias('date'),
-	col('open_ibova').alias('open_ibova'),
-	col('open_oil').alias('open_oil'),
-	col('open_usd').alias('open_usd'),
-	col('open_abev').alias('open_abev'),
-	col('open_jbs').alias('open_jbs'),
-	col('open_petr4').alias('open_petr4'),
-	col('open_vale').alias('open_vale')
+    col("date_ibova").alias("date"),
+    col("open_ibova").alias("open_ibova"),
+    col("open_oil").alias("open_oil"),
+    col("open_usd").alias("open_usd"),
+    col("open_abev").alias("open_abev"),
+    col("open_jbs").alias("open_jbs"),
+    col("open_petr4").alias("open_petr4"),
+    col("open_vale").alias("open_vale"),
 )
 
 # df.write.format('csv').save('ibova_oil_usd.csv',header = 'true')
-df.repartition(1).write.format('com.databricks.spark.csv').save("myfile.csv",header = 'true')
-
+df.repartition(1).write.format("com.databricks.spark.csv").save(
+    "myfile.csv", header="true"
+)
